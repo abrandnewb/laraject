afb4749aa4dfa69236e0e9b642a6735e16cf09a0
Biniyam <abrandnewb@gmail.com>
2019-02-18 11:19:00 +0100

    edit & delete operations

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,4 +1,4 @@
- @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
 <style>
@@ -8,18 +8,19 @@
 </style>
 <div class="card uper">
     <div class="card-header">
-        <h1>Create Post</h1>
+        <h1>Edit Post</h1>
     </div>
   <div class="card-body">
-      <form method="post" action="{{action('postsController@store')}}">
+      <form method="post" action="{{action('postsController@update', $post->id)}}">
+        @method('PATCH')
         @csrf
           <div class="form-group">
               <label for="title">Title:</label>
-              <input type="text" class="form-control" name="title" value="{{ old('title') }}"/>
+              <input type="text" class="form-control" name="title" value="{{ $post->title }}"/>
           </div>
           <div class="form-group">
               <label for="body">Body:</label>
-              <textarea id="article-ckeditor" class="form-control" name="body">{{ old('body') }}</textarea>
+              <textarea id="article-ckeditor" class="form-control" name="body">{{ $post->body }}</textarea>
           </div>
           <button type="submit" class="btn btn-primary">save</button>
       </form>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,13 @@
 @section('content')
 <a href="/posts" class="btn btn-default">Back</a>
 <h1>{{$post->title}}</h1>
-<small>Created at: {{$post->created_at}}</small>
+<small>Created at: {{ date('F d, Y', strtotime($post->created_at)) }}</small>
 <p>{!!$post->body!!}</p>
+<a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+
+<form method="post" action="{{action('postsController@destroy', $post->id)}}" class="pull-right">
+    @csrf
+    @method('DELETE')
+    <button type="submit" class="btn btn-danger">Delete</button>
+</form>
 @endsection
